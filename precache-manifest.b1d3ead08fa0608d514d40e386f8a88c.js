89f63557b0355e58e4930bd202365aa00b96923d
ayyoub <ayyoub@hotmail.com>
2019-10-17 21:15:42 +0200

    Updates

---
+++ precache-manifest.b1d3ead08fa0608d514d40e386f8a88c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9333af8835a1201bdc3e9b51d2c8300",
+    "url": "/Ana-margarida-portfolio-website/index.html"
+  },
+  {
+    "revision": "0a6750404c245692852e",
+    "url": "/Ana-margarida-portfolio-website/static/css/main.1dafce54.chunk.css"
+  },
+  {
+    "revision": "a3d6fcb8bde80c5c96bf",
+    "url": "/Ana-margarida-portfolio-website/static/js/2.121f7309.chunk.js"
+  },
+  {
+    "revision": "0a6750404c245692852e",
+    "url": "/Ana-margarida-portfolio-website/static/js/main.e9bed397.chunk.js"
+  },
+  {
+    "revision": "c25e712a6fcd8615391e",
+    "url": "/Ana-margarida-portfolio-website/static/js/runtime-main.7e0e140c.js"
+  }
+]);
